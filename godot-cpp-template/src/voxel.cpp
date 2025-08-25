3476cc501dca7c33098c7550bc5412e8a6899d76
Hari Krishnan <codeharik@gmail.com>
2025-08-25 20:05:35 +0530

    Refactor

--- godot-cpp-template/src/voxel.cpp
+++ godot-cpp-template/src/voxel.cpp
@@ -7,29 +7,39 @@
 #include <godot_cpp/godot.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
 
+#include <godot_cpp/classes/array_mesh.hpp>
+#include <godot_cpp/variant/packed_int32_array.hpp>
+#include <godot_cpp/variant/packed_vector3_array.hpp>
+
+#include <godot_cpp/classes/surface_tool.hpp>
+
 #include <godot_cpp/classes/material.hpp>
 #include <godot_cpp/classes/mesh.hpp>
 #include <godot_cpp/classes/mesh_instance3d.hpp>
 #include <godot_cpp/classes/packed_scene.hpp>
 #include <godot_cpp/classes/resource_loader.hpp>
 
+#include <godot_cpp/classes/fast_noise_lite.hpp>
+#include <godot_cpp/classes/shader.hpp>
+#include <godot_cpp/classes/shader_material.hpp>
+
 using namespace godot;
 
-void VoxelNode::_bind_methods() {
-}
-
 VoxelNode::VoxelNode() {
-	if (Engine::get_singleton()->is_editor_hint()) {
-		set_process_mode(Node::ProcessMode::PROCESS_MODE_DISABLED);
-	}
-
-	blendTest();
+	set_process_mode(Node::ProcessMode::PROCESS_MODE_ALWAYS);
 }
 
 VoxelNode::~VoxelNode() {
 }
 
 void VoxelNode::_process(double delta) {
+	if (terrain_dirty) {
+		generate_terrain();
+		terrain_dirty = false;
+	}
+}
+
+void VoxelNode::_ready() {
 }
 
 void VoxelNode::blendTest() {
@@ -75,4 +85,135 @@ void VoxelNode::blendTest() {
 			scene_root->queue_free();
 		}
 	}
+}
+
+void VoxelNode::make_triangle() {
+	Ref<ArrayMesh> mesh;
+	mesh.instantiate();
+
+	// 3 vertices
+	PackedVector3Array vertices;
+	vertices.push_back(Vector3(0, 1, 0));
+	vertices.push_back(Vector3(-1, -1, 0));
+	vertices.push_back(Vector3(1, -1, 0));
+
+	// Indices for a single triangle
+	PackedInt32Array indices;
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
+
+	// Build surface array
+	Array arrays;
+	arrays.resize(Mesh::ARRAY_MAX);
+	arrays[Mesh::ARRAY_VERTEX] = vertices;
+	arrays[Mesh::ARRAY_INDEX] = indices;
+
+	mesh->add_surface_from_arrays(Mesh::PRIMITIVE_TRIANGLES, arrays);
+
+	// Put it in the scene
+	MeshInstance3D *mi = memnew(MeshInstance3D);
+	mi->set_mesh(mesh);
+	mi->set_name("GeneratedTriangle");
+	add_child(mi);
+
+	UtilityFunctions::print("Generated triangle mesh!");
+}
+
+void VoxelNode::make_quad() {
+	Ref<SurfaceTool> st;
+	st.instantiate();
+	st->begin(Mesh::PRIMITIVE_TRIANGLES);
+
+	st->add_vertex(Vector3(-1, 0, -1));
+	st->add_vertex(Vector3(1, 0, -1));
+	st->add_vertex(Vector3(1, 0, 1));
+
+	st->add_vertex(Vector3(-1, 0, -1));
+	st->add_vertex(Vector3(1, 0, 1));
+	st->add_vertex(Vector3(-1, 0, 1));
+
+	Ref<ArrayMesh> mesh = st->commit();
+
+	MeshInstance3D *mi = memnew(MeshInstance3D);
+	mi->set_mesh(mesh);
+	mi->set_name("GeneratedQuad");
+	add_child(mi);
+
+	UtilityFunctions::print("Generated quad mesh!");
+}
+
+void VoxelNode::generate_terrain() {
+	for (int i = get_child_count() - 1; i >= 0; --i) {
+		Node *child = get_child(i);
+		remove_child(child);
+		child->queue_free();
+	}
+
+	Ref<FastNoiseLite> noise;
+	noise.instantiate();
+	noise->set_noise_type(FastNoiseLite::TYPE_PERLIN);
+	noise->set_seed(1337);
+	noise->set_frequency(terrain_scale);
+
+	PackedVector3Array vertices;
+	PackedInt32Array indices;
+
+	// Generate vertices
+	for (int z = 0; z < terrain_depth; ++z) {
+		for (int x = 0; x < terrain_width; ++x) {
+			float y = noise->get_noise_2d((float)x, (float)z) * terrain_height_scale;
+			vertices.push_back(Vector3((float)x, y, (float)z));
+		}
+	}
+
+	// Generate indices (two triangles per quad)
+	for (int z = 0; z < terrain_depth - 1; ++z) {
+		for (int x = 0; x < terrain_width - 1; ++x) {
+			int i0 = x + z * terrain_width;
+			int i1 = (x + 1) + z * terrain_width;
+			int i2 = x + (z + 1) * terrain_width;
+			int i3 = (x + 1) + (z + 1) * terrain_width;
+			// First triangle
+			// First triangle
+			indices.push_back(i0);
+			indices.push_back(i1);
+			indices.push_back(i2);
+			// Second triangle
+			indices.push_back(i1);
+			indices.push_back(i3);
+			indices.push_back(i2);
+		}
+	}
+
+	PackedVector3Array normals;
+	normals.resize(vertices.size());
+	for (int i = 0; i < normals.size(); ++i) {
+		normals[i] = Vector3(0, 1, 0);
+	}
+
+	// Build surface array
+	Array arrays;
+	arrays.resize(Mesh::ARRAY_MAX);
+	arrays[Mesh::ARRAY_VERTEX] = vertices;
+	arrays[Mesh::ARRAY_NORMAL] = normals;
+	arrays[Mesh::ARRAY_INDEX] = indices;
+
+	Ref<ArrayMesh> mesh;
+	mesh.instantiate();
+	mesh->add_surface_from_arrays(Mesh::PRIMITIVE_TRIANGLES, arrays);
+
+	// Create MeshInstance3D
+	MeshInstance3D *mi = memnew(MeshInstance3D);
+	mi->set_mesh(mesh);
+	mi->set_name("NoiseTerrain");
+	add_child(mi);
+
+	// Ref<Shader> shader = ResourceLoader::get_singleton()->load("res://shaders/terrain.gdshader");
+	// Ref<ShaderMaterial> mat;
+	// mat.instantiate();
+	// mat->set_shader(shader);
+	mi->set_material_override(terrain_material);
+
+	UtilityFunctions::print("Generated noise terrain mesh!");
 }
--- godot-cpp-template/src/voxel.h
+++ godot-cpp-template/src/voxel.h
@@ -1,7 +1,10 @@
 #ifndef VoxelNode_CLASS
 #define VoxelNode_CLASS
 
+#include <godot_cpp/classes/material.hpp>
 #include <godot_cpp/classes/node3d.hpp>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/object.hpp>
 
 using namespace godot;
 
@@ -9,16 +12,79 @@ class VoxelNode : public Node3D {
 	GDCLASS(VoxelNode, Node3D);
 
 private:
+	int terrain_width = 32;
+	int terrain_depth = 32;
+	float terrain_scale = 0.15f;
+	float terrain_height_scale = 6.0f;
+	Ref<Material> terrain_material;
+
+	bool terrain_dirty = false;
+
 	void blendTest();
+	void make_triangle();
+	void make_quad();
+	void generate_terrain();
 
 protected:
-	static void _bind_methods();
+	static void _bind_methods() {
+		ClassDB::bind_method(D_METHOD("set_terrain_width", "width"), &VoxelNode::set_terrain_width);
+		ClassDB::bind_method(D_METHOD("get_terrain_width"), &VoxelNode::get_terrain_width);
+		ADD_PROPERTY(PropertyInfo(Variant::INT, "terrain_width"), "set_terrain_width", "get_terrain_width");
+
+		ClassDB::bind_method(D_METHOD("set_terrain_depth", "depth"), &VoxelNode::set_terrain_depth);
+		ClassDB::bind_method(D_METHOD("get_terrain_depth"), &VoxelNode::get_terrain_depth);
+		ADD_PROPERTY(PropertyInfo(Variant::INT, "terrain_depth"), "set_terrain_depth", "get_terrain_depth");
+
+		ClassDB::bind_method(D_METHOD("set_terrain_scale", "scale"), &VoxelNode::set_terrain_scale);
+		ClassDB::bind_method(D_METHOD("get_terrain_scale"), &VoxelNode::get_terrain_scale);
+		ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "terrain_scale"), "set_terrain_scale", "get_terrain_scale");
+
+		ClassDB::bind_method(D_METHOD("set_terrain_height_scale", "height_scale"), &VoxelNode::set_terrain_height_scale);
+		ClassDB::bind_method(D_METHOD("get_terrain_height_scale"), &VoxelNode::get_terrain_height_scale);
+		ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "terrain_height_scale"), "set_terrain_height_scale", "get_terrain_height_scale");
+
+		ClassDB::bind_method(D_METHOD("set_terrain_material", "material"), &VoxelNode::set_terrain_material);
+		ClassDB::bind_method(D_METHOD("get_terrain_material"), &VoxelNode::get_terrain_material);
+		ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "terrain_material", PROPERTY_HINT_RESOURCE_TYPE, "Material"), "set_terrain_material", "get_terrain_material");
+	}
 
 public:
 	VoxelNode();
 	~VoxelNode();
 
 	void _process(double delta) override;
+
+	void _ready() override;
+
+	void set_terrain_width(int w) {
+		terrain_width = w;
+		terrain_dirty = true;
+	}
+	int get_terrain_width() const { return terrain_width; }
+
+	void set_terrain_depth(int d) {
+		terrain_depth = d;
+		terrain_dirty = true;
+	}
+	int get_terrain_depth() const { return terrain_depth; }
+
+	void set_terrain_scale(float s) {
+		terrain_scale = s;
+		terrain_dirty = true;
+	}
+	float get_terrain_scale() const { return terrain_scale; }
+
+	void set_terrain_height_scale(float hs) {
+		terrain_height_scale = hs;
+		terrain_dirty = true;
+	}
+	float get_terrain_height_scale() const { return terrain_height_scale; }
+
+	void set_terrain_material(const Ref<Material> &mat) {
+		terrain_material = mat;
+		terrain_dirty = true;
+	}
+	Ref<Material> get_terrain_material() const { return terrain_material; }
 };
 
 #endif
