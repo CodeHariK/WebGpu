8e5e2c5236a9519b41c56c40353d03a2ce26a5ff
Hari Krishnan <codeharik@gmail.com>
2025-10-25 21:08:06 +0530

    Refactor

--- minecraft/project/scripts/noise_map/biome.gd
+++ minecraft/project/scripts/noise_map/biome.gd
@@ -16,52 +16,78 @@ extends TextureRect
         altitude_noise = value
         if Engine.is_editor_hint():
             draw_map()
+@export var river_noise: FastNoiseLite:
+    set(value):
+        river_noise = value
+        if Engine.is_editor_hint():
+            draw_map()
 
-@export var width = 512
-@export var height = 512
+# New Biome Colors based on your request
+const COLOR_OCEAN = Color("00008b") # DarkBlue
+const COLOR_LAKE = Color.PINK # RoyalBlue
 
-# Biome Colors
+const COLOR_BEACH = Color("fafad2") # LightGoldenrodYellow
+const COLOR_JUNGLE = Color.LIGHT_GREEN # ForestGreen
+
+const COLOR_PLAINS = Color.MAGENTA # Color("7cfc00") # LawnGreen
+
+const COLOR_DESERT = Color.BLACK # Color("f4a460") # SandyBrown
+const COLOR_MOUNTAIN = Color("808080") # Gray
 const COLOR_SNOW = Color.WHITE
-const COLOR_MOUNTAIN = Color.GRAY
-const COLOR_DESERT = Color.YELLOW # Sandy Brown
-const COLOR_ARID = Color.MAROON # Peru
-const COLOR_MARSHY = Color.LIME_GREEN # Sea Green
-const COLOR_DARK_FOREST = Color.DARK_GREEN # Dark Green
-const COLOR_SPRING = Color.PINK # Lawn Green
-const COLOR_WATER = Color.CYAN # Cornflower Blue
 
-func get_biome(temp, moist, alt):
-    # Altitude-based biomes take priority
-    if alt > 0.75:
-        return COLOR_MOUNTAIN
-    if alt < 0.2:
-        return COLOR_WATER
+func get_biome(temp, moist, alt, river):
+    # return Color(temp, temp, temp)
+    # return Color(moist, moist, moist)
+    # return Color(alt, alt, alt)
+    # return Color(alt * river, alt * river, alt * river)
+    var altriver = 1 if alt * river > 0.1 else 0
+    return Color(altriver, altriver, altriver)
 
-    # Temperature-based biomes
-    if temp < 0.2:
-        return COLOR_SNOW
-    if temp > 0.8:
-        if moist < 0.25:
-            return COLOR_DESERT
-        else:
-            return COLOR_ARID # Hot but not dry enough for desert
+    # # Ocean
+    # if river < 0.3:
+    #     return COLOR_OCEAN
+    
+    # # Beach
+    # elif river >= 0.3 and river < 0.5:
+    #     return COLOR_BEACH
 
-    # Moisture and Temperature combined
-    if moist > 0.7:
-        if temp < 0.6:
-            return COLOR_MARSHY
+    # return Color.BLACK
+    
+    # # Land Biomes
+    # elif alt >= 0.5 and alt < 0.8:
+    #     # Lakes (high moisture overrides other land biomes)
+    #     if moist >= 0.9:
+    #         return COLOR_LAKE
 
-    if moist > 0.4 and temp < 0.4:
-        return COLOR_DARK_FOREST
-
-    # Default biome
-    return COLOR_SPRING
+    #     # Jungle (hot and wet)
+    #     elif temp > 0.6 and moist >= 0.4 and moist < 0.9:
+    #         return COLOR_JUNGLE
+    #     # Desert (hot and dry)
+    #     elif temp > 0.6 and moist < 0.4:
+    #         return COLOR_DESERT
+    #     # Plains (temperate)
+    #     else: # temp <= 0.6
+    #         return COLOR_PLAINS
+            
+    # # Mountains
+    # elif alt >= 0.8 and alt < 0.95:
+    #     return COLOR_MOUNTAIN
+    # # Snow
+    # else: # alt >= 0.95
+    #     return COLOR_SNOW
 
 func draw_map():
     if not temperature_noise or not moisture_noise or not altitude_noise:
         return
 
-    var image = Image.create(width, height, false, Image.FORMAT_RGB8)
+    var current_size = get_size()
+    var width = int(current_size.x)
+    var height = int(current_size.y)
+
+    if width <= 0 or height <= 0:
+        return
+
+    var image = Image.create_empty(width, height, false, Image.FORMAT_RGB8)
     
     for x in width:
         for y in height:
@@ -69,8 +95,9 @@ func draw_map():
             var temp = (temperature_noise.get_noise_2d(x, y) + 1.0) / 2.0
             var moist = (moisture_noise.get_noise_2d(x, y) + 1.0) / 2.0
             var alt = (altitude_noise.get_noise_2d(x, y) + 1.0) / 2.0
+            var river = (river_noise.get_noise_2d(x, y) + 1.0) / 2.0
             
-            var biome_color = get_biome(temp, moist, alt)
+            var biome_color = get_biome(temp, moist, alt, river)
             image.set_pixel(x, y, biome_color)
 
     var image_texture = ImageTexture.create_from_image(image)
@@ -82,13 +109,9 @@ func _ready():
     if not moisture_noise: moisture_noise = FastNoiseLite.new()
     if not altitude_noise: altitude_noise = FastNoiseLite.new()
     
+    # Connect the resized signal to automatically redraw the map when the control's size changes.
+    if not is_connected("resized", Callable(self, "draw_map")):
+        connect("resized", Callable(self, "draw_map"))
+
     # Create and display the biome map
     draw_map()
-
-func _input(event):
-    if event.is_action_pressed("ui_accept"):
-        # Re-seed the noise for a new map and redraw
-        temperature_noise.seed = randi()
-        moisture_noise.seed = randi()
-        altitude_noise.seed = randi()
-        draw_map()
