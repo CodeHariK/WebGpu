329327914e64b99ee049a8a98a0ca8077eab1337
Hari Krishnan <144345505+CodeHariK@users.noreply.github.com>
2026-08-11 23:13:01 +0530

    Refactor

--- MechAnim/operators/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Module Path: MechAnim/operators/__init__.py
-System Responsibility: Package marker for operator components.
-Build Dependencies: None
-"""
--- MechAnim/ui/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Module Path: MechAnim/ui/__init__.py
-System Responsibility: Package marker for UI components.
-Build Dependencies: None
-"""
